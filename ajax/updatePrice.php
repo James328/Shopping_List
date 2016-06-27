b07464f6a5602021fdc6ffa11ae07402840eaf4b
James Hume <jameshume328@gmail.com>
2016-06-27 18:27:28 -0500

    removing old code that has moved

--- ajax/updatePrice.php
+++ ajax/updatePrice.php
@@ -1,15 +1,4 @@
 <?php 
 require_once '../includes/db.php'; // The mysql database connection script
-if(isset($_GET['itemID']) && isset($_GET['price']))
-{
-	$itemID = $mysqli->real_escape_string($_GET['itemID']);
-	$price = $mysqli->real_escape_string($_GET['price']);
 
-	$query="UPDATE shop set price='$price' where id='$itemID'";
-
-	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-	$result = $mysqli->affected_rows;
-
-	echo $json_response = json_encode($result);
-}
 ?>
