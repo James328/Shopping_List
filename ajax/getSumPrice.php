b2875db88c974f88489c78ed83a57270fc782971
James Hume <jameshume328@gmail.com>
2016-07-20 22:45:45 -0500

    update getSumPrice() to calculate for currently active list

--- ajax/getSumPrice.php
+++ ajax/getSumPrice.php
@@ -1,7 +1,30 @@
 <?php 
 require_once '../includes/db.php'; // The mysql database connection script
 
-$query="SELECT SUM(PRICE * QUANTITY) as PRICE from shop";
+$user_id = '1';	// manually setting the user ID for now, which also means
+						// $active_list is manually set in db, currently == 1
+
+// pulling the active_list
+$query="SELECT ID, ACTIVE_LIST as active_list from user where id='$user_id'";
+$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
+
+$active_list = $arr[0]['active_list'];
+
+if($active_list == '1')
+{
+	$query="SELECT SUM(PRICE * QUANTITY) as PRICE from shop";
+}
+else
+{
+	$query="SELECT SUM(PRICE * QUANTITY) as PRICE from shop WHERE LIST_ID='$active_list'";
+}
 $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
 
