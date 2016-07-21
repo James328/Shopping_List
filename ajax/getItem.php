b3675a2a46f6fae999c134ea46c3ab0943aa7cf1
James Hume <jameshume328@gmail.com>
2016-07-20 21:44:13 -0500

    added user table to db, starting to filter items based on selected active_list

--- ajax/getItem.php
+++ ajax/getItem.php
@@ -6,7 +6,26 @@ if(isset($_GET['status']))
 	$status = $mysqli->real_escape_string($_GET['status']);
 }
 
-$query="SELECT ID, ITEM, STATUS, CREATED_AT, QUANTITY, PRICE, LIST_ID, NOTES from shop where status like '$status' order by status,id desc, quantity asc";
+$id = '1';	// manually setting the user ID for now, which also means
+						// $active_list is manually set in db, currently == 1
+
+// pulling the active_list
+$query="SELECT ID, ACTIVE_LIST as active_list from user where id='$id'";
+
+$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
+
+$active_list = $arr[0]['active_list'];
+
+$query="SELECT ID, ITEM, STATUS, CREATED_AT, QUANTITY, PRICE, LIST_ID, NOTES from shop where LIST_ID='$active_list' order by status,id desc, quantity asc";
+//$query="SELECT ID, ITEM, STATUS, CREATED_AT, QUANTITY, PRICE, LIST_ID, NOTES from shop where status like '$status' order by status,id desc, quantity asc";
+
 $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
 $arr = array();
--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 14, 2016 at 11:42 PM
+-- Generation Time: Jul 21, 2016 at 04:42 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.7
 
@@ -50,20 +50,40 @@ CREATE TABLE `shop` (
   `quantity` int(11) NOT NULL,
   `price` decimal(4,2) DEFAULT NULL,
   `price_type` int(11) DEFAULT NULL,
-  `list_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `list_id` int(11) DEFAULT NULL,
+  `notes` longtext COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `shop`
 --
 
-INSERT INTO `shop` (`id`, `item`, `status`, `created_at`, `quantity`, `price`, `price_type`, `list_id`) VALUES
-(15, 'Bread', 0, '2015-05-13', 1, '5.00', 0, NULL),
-(24, 'Apples', 0, '2016-06-29', 2, '12.00', 0, NULL),
-(25, 'Oranges', 0, '2016-06-29', 1, NULL, 2, 3),
-(26, 'Pears', 0, '2016-06-29', 6, NULL, 2, NULL),
-(27, 'Beer', 0, '2016-06-29', 3, '5.00', 2, 1),
-(28, 'Strawberry', 0, '2016-07-14', 4, '5.00', NULL, NULL);
+INSERT INTO `shop` (`id`, `item`, `status`, `created_at`, `quantity`, `price`, `price_type`, `list_id`, `notes`) VALUES
+(15, 'Bread', 0, '2015-05-13', 1, '5.00', 0, 1, ''),
+(24, 'Apples', 0, '2016-06-29', 4, '1.99', 0, NULL, ''),
+(25, 'Oranges', 0, '2016-06-29', 4, '1.00', 2, 1, ''),
+(26, 'Pears', 2, '2016-06-29', 3, '0.89', 2, NULL, ''),
+(27, 'Beer', 0, '2016-06-29', 1, '5.99', 2, 1, 'Fat tire pls'),
+(30, 'Strawberries', 0, '2016-07-20', 3, NULL, NULL, NULL, ''),
+(31, 'Eggs', 0, '2016-07-20', 1, NULL, NULL, 1, 'Make sure to get the large size');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `active_list` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `active_list`) VALUES
+(1, 1);
 
 --
 -- Indexes for dumped tables
@@ -82,6 +102,12 @@ ALTER TABLE `shop`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -89,4 +115,4 @@ ALTER TABLE `shop`
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
