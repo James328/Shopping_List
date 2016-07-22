ded6e9c45f4d6f924f6c2c49f4007a163487603c
James Hume <jameshume328@gmail.com>
2016-07-21 21:46:30 -0500

    reorganizing functions

--- app/app.js
+++ app/app.js
@@ -8,114 +8,114 @@ app.controller('shopController', function($scope, $http) {
   getSumPrice();  // Sum all of the prices
  
   function getItem(){  
-  $http.post("ajax/getItem.php").success(function(data){
-        $scope.items = data;
-        getSumPrice();
-       });
+    $http.post("ajax/getItem.php").success(function(data){
+      $scope.items = data;
+      getSumPrice();
+    });
   };
 
-  function getList(){  
-  $http.post("ajax/getList.php").success(function(data){
-        $scope.lists = data;
-       });
-  };
-
-  function getSumPrice(){
-  $http.post("ajax/getSumPrice.php").success(function(data){
-        $scope.sumPrice = data;
-      });
-  };
-  
   $scope.addItem = function (item, quantity) {
     $http.post("ajax/addItem.php?item="+item+"&quantity="+quantity).success(function(data){
-        getItem();
-        $scope.itemInput = "";
-      });
-  };
-
-  $scope.addList = function (list) {
-    $http.post("ajax/addList.php?list="+list).success(function(data){
-        getList();
-        $scope.listInput = "";
-      });
+      getItem();
+      $scope.itemInput = "";
+    });
   };
 
   $scope.deleteItem = function (item) {
     if(confirm("Are you sure to delete this item?")){
-    $http.post("ajax/deleteItem.php?itemID="+item).success(function(data){
+      $http.post("ajax/deleteItem.php?itemID="+item).success(function(data){
         getItem();
-      });
-    }
-  };
-
-  $scope.deleteList = function (list) {
-    if(confirm("Are you sure to delete this item?")){
-    $http.post("ajax/deleteList.php?listID="+list).success(function(data){
-        getList();
       });
     }
   };
 
   $scope.clearItem = function () {
     if(confirm("Delete all checked items?")){
-    $http.post("ajax/clearItem.php").success(function(data){
+      $http.post("ajax/clearItem.php").success(function(data){
         getItem();
       });
     }
-  };  
+  };
 
   $scope.changeStatus = function(item, status) {
     if(status=='2'){status='0';}else{status='2';}
-      $http.post("ajax/updateItem.php?itemID="+item+"&status="+status).success(function(data){
-        getItem();
+    $http.post("ajax/updateItem.php?itemID="+item+"&status="+status).success(function(data){
+      getItem();
+    });
+  };
+
+  function getList(){  
+    $http.post("ajax/getList.php").success(function(data){
+      $scope.lists = data;
+    });
+  };
+
+  $scope.addList = function (list) {
+    $http.post("ajax/addList.php?list="+list).success(function(data){
+      getList();
+      $scope.listInput = "";
+    });
+  };
+
+  $scope.deleteList = function (list) {
+    if(confirm("Are you sure to delete this item?")){
+      $http.post("ajax/deleteList.php?listID="+list).success(function(data){
+        getList();
       });
+    }
   };
 
   $scope.changeList = function(list) {
-      $http.post("ajax/updateList.php?list="+list).success(function(data){
-        getItem();
-      });
+    $http.post("ajax/updateList.php?list="+list).success(function(data){
+      getItem();
+    });
+  };
+
+  function getSumPrice(){
+    $http.post("ajax/getSumPrice.php").success(function(data){
+      $scope.sumPrice = data;
+    });
   };
 
   $scope.addPrice = function (item, price) {
     $http.post("ajax/addPrice.php?itemID="+item+"&price="+price).success(function(data){
-        getItem();
-        $scope.itemInput = "";
-      });
+      getItem();
+      $scope.itemInput = "";
+    });
   };
 
   $scope.increaseQuantity = function(item) {
-      $http.post("ajax/increaseQuantity.php?itemID="+item).success(function(data){
-        getItem();
-      });
+    $http.post("ajax/increaseQuantity.php?itemID="+item).success(function(data){
+      getItem();
+    });
   };
 
   $scope.decreaseQuantity = function(item) {
-      $http.post("ajax/decreaseQuantity.php?itemID="+item).success(function(data){
-        getItem();
-      });
+    $http.post("ajax/decreaseQuantity.php?itemID="+item).success(function(data){
+      getItem();
+    });
   };
 
   $scope.showPriceForm = function(item) {
-      $("#form-"+item).css("display","inline-block");
-      $(".unit-price-"+item).css("display","none");
-      $("#edit-price-btn-"+item).css("display","none");
-      $("#close-price-btn-"+item).css("display","inline-block");
+    $("#form-"+item).css("display","inline-block");
+    $(".unit-price-"+item).css("display","none");
+    $("#edit-price-btn-"+item).css("display","none");
+    $("#close-price-btn-"+item).css("display","inline-block");
   };
 
    $scope.hidePriceForm = function(item) {
-      $("#form-"+item).css("display","none");
-      $(".unit-price-"+item).css("display","inline-block");
-      $("#edit-price-btn-"+item).css("display","inline-block");
-      $("#close-price-btn-"+item).css("display","none");
+    $("#form-"+item).css("display","none");
+    $(".unit-price-"+item).css("display","inline-block");
+    $("#edit-price-btn-"+item).css("display","inline-block");
+    $("#close-price-btn-"+item).css("display","none");
   };
 
   $scope.addNotes = function (item, notes) {
     $http.post("ajax/addNotes.php?itemID="+item+"&notes="+notes).success(function(data){
-        getItem();
-        $scope.notesInput = "";
-      });
-      $("#notesModal-"+item).modal("hide");
+      getItem();
+      $scope.notesInput = "";
+    });
+    $("#notesModal-"+item).modal("hide");
   };
 
 });
