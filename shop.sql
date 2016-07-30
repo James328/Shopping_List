42ebd92044e14db11312c41ed63af7e5f12148c5
James Hume <jameshume328@gmail.com>
2016-07-30 17:10:06 -0500

    updating sql dump

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 27, 2016 at 07:25 AM
+-- Generation Time: Jul 31, 2016 at 12:09 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.7
 
@@ -31,19 +31,18 @@ CREATE TABLE `item` (
   `price` decimal(4,2) DEFAULT NULL,
   `price_type` int(11) DEFAULT NULL,
   `notes` longtext COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`item_id`, `list_id`, `user_id`, `name`, `created_at`, `status`, `quantity`, `price`, `price_type`, `notes`) VALUES
-(15, 2, 0, 'Bread', '2016-07-26 04:36:26', 0, 1, '5.00', 0, ''),
-(24, 3, 0, 'Hammer', '2016-07-26 04:36:26', 0, 1, '5.99', 0, ''),
-(25, 2, 0, 'Oranges', '2016-06-29 05:00:00', 0, 4, '1.00', 2, ''),
-(27, 2, 0, 'Beer', '2016-07-26 04:37:43', 0, 1, '6.99', 2, 'Fat tire pls'),
-(31, 2, 0, 'Eggs', '2016-07-24 23:05:18', 0, 1, '5.99', NULL, 'Make sure to get the large size'),
-(36, 0, 0, 'Prescriptions', '2016-07-27 04:44:59', 0, 3, NULL, NULL, 'Make sure to get generics');
+(24, 3, 1, 'Hammer', '2016-07-30 18:29:18', 0, 1, '5.99', 0, ''),
+(25, 2, 1, 'Oranges', '2016-07-30 18:29:19', 0, 4, '1.00', 2, ''),
+(27, 2, 1, 'Beer', '2016-07-30 18:29:19', 0, 1, '6.99', 2, 'Fat tire pls'),
+(31, 2, 1, 'Eggs', '2016-07-30 18:29:20', 0, 1, '5.99', NULL, 'Make sure to get the large size'),
+(36, 0, 1, 'Prescriptions', '2016-07-30 18:29:21', 0, 3, NULL, NULL, 'Make sure to get generics');
 
 -- --------------------------------------------------------
 
@@ -56,17 +55,18 @@ CREATE TABLE `list` (
   `user_id` mediumint(8) unsigned NOT NULL,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `list`
 --
 
 INSERT INTO `list` (`list_id`, `user_id`, `name`, `created_at`) VALUES
-(1, 0, 'Show All', '2016-07-26 04:31:52'),
-(2, 0, 'Grocery Store', '2016-07-24 04:26:03'),
-(3, 0, 'Hardware Store', '2016-07-24 04:26:04'),
-(4, 0, 'Other', '2016-07-24 04:26:05');
+(1, 1, 'Show All', '2016-07-30 18:28:49'),
+(2, 1, 'Grocery Store', '2016-07-30 18:28:50'),
+(3, 1, 'Hardware Store', '2016-07-30 18:28:50'),
+(4, 1, 'Other', '2016-07-30 18:28:51'),
+(6, 1, 'Paws list', '2016-07-30 18:28:52');
 
 -- --------------------------------------------------------
 
@@ -89,7 +89,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `active_list`, `first_name`, `last_name`, `email`, `pass`, `created_at`) VALUES
-(1, 1, 'James', 'Hume', 'james@hume.com', '1234', '2016-07-27 05:24:38');
+(1, 1, 'James', 'Hume', 'james@hume.com', '1234', '2016-07-30 21:16:30');
 
 --
 -- Indexes for dumped tables
@@ -99,9 +99,7 @@ INSERT INTO `user` (`user_id`, `active_list`, `first_name`, `last_name`, `email`
 -- Indexes for table `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`item_id`),
-  ADD KEY `item_list` (`list_id`),
-  ADD KEY `item_user` (`user_id`);
+  ADD PRIMARY KEY (`item_id`);
 
 --
 -- Indexes for table `list`
@@ -113,8 +111,7 @@ ALTER TABLE `list`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `user_active_list` (`active_list`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -124,12 +121,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=37;
+  MODIFY `item_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
 --
 -- AUTO_INCREMENT for table `list`
 --
 ALTER TABLE `list`
-  MODIFY `list_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `list_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `user`
 --
